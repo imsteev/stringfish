65548fd40e01c059b405a618920066b65ecf1419
Stephen Chung <whosteev95@gmail.com>
2023-04-16 19:55:35 -0400

    more log

--- middleware.go
+++ middleware.go
@@ -7,6 +7,7 @@ import (
 
 func LogHandler(f func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Header)
 		fmt.Printf("%s %s \n", r.Method, r.URL.Path)
 		f(w, r)
 	}
